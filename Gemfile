6fd7aa478115c1f1042104105d8dc620b20a5549
Thomas Reeves <treeves@northislandcollege.ca>
2018-04-10 02:39:57 -0700

    articles added to view

--- Gemfile
+++ Gemfile
@@ -8,9 +8,10 @@ gem 'rails', '~> 5.2.0'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
-gem 'puma', '~> 3.11'
+gem 'bootstrap-sass', '~> 3.3.5'
+gem 'sass-rails', '>= 3.2'
 # Use SCSS for stylesheets
-gem 'sass-rails', '~> 5.0'
+
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -7,9 +7,11 @@ class HomepageController < ApplicationController
     @articles = Article.all
   end
 
+  private
+
   def get_news
     require "open-uri"
-    url = "https://newsapi.org/v2/everything?domains=wsj.com,nytimes.com&apiKey=b4e01154f2ca4fb5889b640bce9e15d4"
+    url = "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=b4e01154f2ca4fb5889b640bce9e15d4"
     response = RestClient.get(url)
     @hash = JSON.parse(response)
     @hash["articles"].each do |article|
